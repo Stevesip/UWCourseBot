34ea81fbaeb9257dec4f96d3060fb39e37f09e18
Clarence Chan <clarencejy1234@gmail.com>
2016-06-25 14:23:23 -0400

    Port Update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -5,5 +6,5 @@ app = Flask(__name__)
 def hello():
     return "Hello world!"
 
-if __name__ == "__main__":
-    app.run(debug = True)
+ port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
