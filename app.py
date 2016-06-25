e490afa5209e23f4b31583fbdb3ae5b2d45e8aa9
Clarence Chan <clarencejy1234@gmail.com>
2016-06-25 14:48:22 -0400

    WebhO0k

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+@app.route("/webhook", methods=['GET', 'POST'])
+def webhook():
+	if request.method == "GET":
+			if request.args.get('hub.verify_token') == "enghack":
+				return request.args.get("hub.hallenge")
+			return "Wrong Verify Token"
+		
 
 
+#@app.route("")
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
