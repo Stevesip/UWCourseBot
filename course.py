74450a80044fc5cb6479e91009c9c5deeef8169a
Clarence Chan <clarencejy1234@gmail.com>
2016-06-25 18:29:50 -0400

    Courses Class

--- course.py
+++ course.py
@@ -1,12 +1,25 @@
+import requests
+
+
+key = "a2cd9cdc7b9358e4156850f6e27ca339"
 class Course:
 
 	users = []
+	num = None
+	name = None
+	
 
+	def __init__(self, num, name):
+    	self.num = num
+    	self.name = name
 
-	def __init__(self):
-    	self.
+    def addUser(self, user):
+    	self.users.append(user)
 
     def update():
+    	data = requests.get('https://api.uwaterloo.ca/v2/'self.name+'/'+self.num+'.json?key='+key)
+   	    courseresponse = urllib.urlopen(data)
+        coursedata = json.load(response)
 
 
 
@@ -14,5 +27,5 @@ class Course:
    	def message_send():
 
 
-   	
+
    	def 
