2d51571329d58a32bf10f43eab9f0b67bd3023ba
Nathan Jiahong Liu <zero_2_6@hotmail.com>
2016-06-25 15:26:46 -0700

    Trial 1 response

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request
+import requests
 import json
-<<<<<<< HEAD
-=======
-#import requests
->>>>>>> origin/master
 import traceback
 import random
 import os
@@ -11,10 +8,7 @@ import os
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 token = "enghack"
-=======
->>>>>>> origin/master
 
 
 @app.route("/")
@@ -23,22 +17,14 @@ def root():
 
 @app.route("/webhook", methods=['GET', 'POST'])
 def webhook():
-<<<<<<< HEAD
     if request.method == "POST":
         try:
             data = json.loads(request.data)
-            data['entry'][0]['messaging'][0]['message']['text']
+            text = data['entry'][0]['messaging'][0]['message']['text']
             sender = data['entry'][0]['messaging'][0]['sender']['id']
             payload = {'recipient': {'id': sender}, 'message': {'text': "You sent:" + text }}
              r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
     elif request.method == "GET":
-=======
-	if request.method == "POST":
-		data = json.loads(request.data)
-		print (data)
-
-	if request.method == "GET":
->>>>>>> origin/master
 		if request.args.get("hub.verify_token") == 'enghack':
 			return request.args.get('hub.challenge')
 		return "Wrong Verify Token"
