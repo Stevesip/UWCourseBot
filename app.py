088585c6c3775e163c52911b1150b5fe98ae5d9e
Clarence Chan <clarencejy1234@gmail.com>
2016-06-25 15:09:36 -0400

    WebHook Working

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ import os
 
 app = Flask(__name__)
 
+
+
+@app.route("/")
+def root():
+	return "UWCourseBot"
+
 @app.route("/webhook", methods=['GET', 'POST'])
 def webhook():
 	if request.method == "GET":
@@ -14,9 +20,6 @@ def webhook():
 			return request.args.get('hub.challenge')
 		return "Wrong Verify Token"
 
-
-
-#@app.route("")
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
