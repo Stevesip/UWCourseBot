ed78e3e92b0fef5fb1cb8d6dcd20c290b60c9806
Nathan Jiahong Liu <zero_2_6@hotmail.com>
2016-06-25 15:21:41 -0700

    Return value trials
    see if data is returned

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
+from flask import Flask, request
+import json
+import traceback
+import random
 import os
-from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+token = "enghack"
 
+
+@app.route("/")
+def root():
+	return "UWCourseBot"
+
+@app.route("/webhook", methods=['GET', 'POST'])
+def webhook():
+    if request.method == "POST":
+        try:
+            data = json.loads(request.data)
+            data['entry'][0]['messaging'][0]['message']['text']
+            sender = data['entry'][0]['messaging'][0]['sender']['id']
+            payload = {'recipient': {'id': sender}, 'message': {'text': "You sent:" + text }}
+             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+    elif request.method == "GET":
+		if request.args.get("hub.verify_token") == 'enghack':
+			return request.args.get('hub.challenge')
+		return "Wrong Verify Token"
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
