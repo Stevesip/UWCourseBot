aea5e8f79ee4c5ab3baf5333c54bffc494dcf0aa
Nathan Jiahong Liu <zero_2_6@hotmail.com>
2016-06-25 15:28:05 -0700

    Trial pls

--- app.py
+++ app.py
@@ -31,8 +31,4 @@ def webhook():
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
-<<<<<<< HEAD
 	app.run(host='0.0.0.0', port=port)
-=======
-	app.run(host='0.0.0.0', port=port)
->>>>>>> origin/master
