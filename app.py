f9029e7adc103db9d0d7d7f2d956a879861a2fa0
Nathan Jiahong Liu <zero_2_6@hotmail.com>
2016-06-25 16:20:00 -0700

    checking for user input

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def webhook():
     if request.method == "POST":
         try:
             data = json.loads(request.data)
+            sys.stdout.write(data)
             text = data['entry'][0]['messaging'][0]['message']['text']
             sender = data['entry'][0]['messaging'][0]['sender']['id']
             payload = {'recipient': {'id': sender}, 'message': {'text': text }}
