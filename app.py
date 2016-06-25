b9b4c06d85c875571921d166bf7c67f4fb4c30d8
Clarence Chan <clarencejy1234@gmail.com>
2016-06-25 17:38:38 -0400

    wassup

--- app.py
+++ app.py
@@ -13,13 +13,12 @@ def webhook():
     if request.method == 'POST':
         try:
             data = json.loads(request.data)
-            text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
+            text = data["entry"][0]["messaging"][0]["message"]["text"] # Incoming Message Text
             sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-            payload = {'recipient': {'id': sender}, 'message': {'text': text[::-1] }} # We're going to send this back
+            payload = {'recipient': {'id': sender}, 'message': {'text': text }} # We're going to send this back
             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
         except Exception as e:
             print traceback.format_exc()
-            sys.stdout.write(json.dumps(data))
              # something went wrong
         return "ok"
     elif request.method == 'GET': # For the initial verification
