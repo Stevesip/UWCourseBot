c60d43b974832517dbf0e5b1af6b328bfe577500
Nathan Jiahong Liu <zero_2_6@hotmail.com>
2016-06-25 22:02:38 -0700

    trying ai implementation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from wit import Wit
 import requests
 import os
 import sys
@@ -7,6 +8,29 @@ import traceback
 import random
 import urllib
 app = Flask(__name__)
+
+def fetchCourse(session_id, context):
+
+
+def say(session_id, context, msg):
+    print(msg)
+
+def merge(session_id, context, entities, msg):
+    return context
+
+def error(session_id, context, e):
+    print(str(e))
+
+actions = {
+    'say': say,
+    'merge': merge,
+    'error': error,
+    'fetchCourse': fetchCourse,
+}
+
+aitoken = 'OVCKIDGTEFCPY3P4DL3XBA25H7QUWWWJ'
+
+client = Wit(aitoken, actions)
 
 key = "a2cd9cdc7b9358e4156850f6e27ca339"
 token = "EAAWx45TcH2oBAG7oZAtIoljLsiyQ8rrOlZC1LdXoaAEKau5YBfhrR5LLJnWegJ5VZAlRj98hm4xa2SIBg67aKYpqZBFwvWJAzD8pJ01zxR4qF8HaRXBDWsvZAIrZAZABsADJqfG537eEREziF87b5d1vnkNZCBF7sUBaLHsTCxul5wZDZD"
@@ -20,15 +44,13 @@ def webhook():
             sys.stdout.write("Got data!")
             sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
             text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
-            sys.stdout.write('got text and sender\n')
-            courseinfo = text.split()
-            payload = {'recipient': {'id': sender}, 'message': {'text': "You have selected:" + courseinfo[0] + " " + courseinfo[1] +". Retrieving data!" }}
-            sys.stdout.write(courseinfo[0])
-            r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
-            getcoursesinfo = 'https://api.uwaterloo.ca/v2/courses/'+ courseinfo[0] + '/' + courseinfo[1] + '.json?key=' + key
-            courseresponse = urllib.urlopen(getcoursesinfo)
-            coursedata = json.load(courseresponse)
-            returntext = "The course title is:" + coursedata['data']['title']
+            resp = client.converse('my-user-session-42', text, {})
+            sys.stdout.write(str(resp))
+
+            #getcoursesinfo = 'https://api.uwaterloo.ca/v2/courses/'+ courseinfo[0] + '/' + courseinfo[1] + '.json?key=' + key
+            #courseresponse = urllib.urlopen(getcoursesinfo)
+            #coursedata = json.load(courseresponse)
+            #returntext = "The course title is:" + coursedata['data']['title']
             #flag = 0
             #for c in totalcourses:
             #    if (text == c.title)
@@ -39,7 +61,7 @@ def webhook():
             #for c in totalcourses:
             #    if c.title == text:
             #        returntext = c.update()
-            payload = {'recipient': {'id': sender}, 'message': {'text': returntext }} # We're going to send this back
+            payload = {'recipient': {'id': sender}, 'message': {'text': str(resp) }} # We're going to send this back
             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
         except Exception as e:
             print traceback.format_exc() # something went wrong
