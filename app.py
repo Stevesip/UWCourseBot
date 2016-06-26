a8048cfcb271c4e9abeeaf08e93d2eb2a50e1007
Nathan Jiahong Liu <zero_2_6@hotmail.com>
2016-06-25 22:11:34 -0700

    done

--- app.py
+++ app.py
@@ -76,7 +76,8 @@ def webhook():
             text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
             resp = client.converse(sender, text, {})
             sys.stdout.write(str(resp))
-            payload = {'recipient': {'id': sender}, 'message': {'text': str(resp) }} # We're going to send this 
+            payload = {'recipient': {'id': sender}, 'message': {'text': str(resp) }} # We're going to send this back
+            r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
         except Exception as e:
             print traceback.format_exc() # something went wrong
         return "ok"
