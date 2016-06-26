aaec1e3b6eb19f018baba24f21df85ed037b01ca
Nathan Jiahong Liu <zero_2_6@hotmail.com>
2016-06-25 18:13:53 -0700

    Revert "uploaded some data"
    This reverts commit 340ecb4ffeaa9890f243b4436cd6d4a0dee507f9.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import random
 import sys
 app = Flask(__name__)
 
-key = "a2cd9cdc7b9358e4156850f6e27ca339"
 token = "EAAWx45TcH2oBAG7oZAtIoljLsiyQ8rrOlZC1LdXoaAEKau5YBfhrR5LLJnWegJ5VZAlRj98hm4xa2SIBg67aKYpqZBFwvWJAzD8pJ01zxR4qF8HaRXBDWsvZAIrZAZABsADJqfG537eEREziF87b5d1vnkNZCBF7sUBaLHsTCxul5wZDZD"
 
 @app.route('/webhook', methods=['GET', 'POST'])
@@ -15,13 +14,8 @@ def webhook():
         try:
             data = json.loads(request.data)
             text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
-            courseinfo = text.split()
-            getcoursesinfo = requests.get('https://api.uwaterloo.ca/v2/'courseinfo[0]+'/'+courseinfo[1]+'.json?key='+key)
-            courseresponse = urllib.urlopen(getcoursesinfo)
-            coursedata = json.load(response)
-            returntext = "You have chosen" + text "," + "The course title is:" + coursedata['data']['title']
             sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-            payload = {'recipient': {'id': sender}, 'message': {'text': returntext] }} # We're going to send this back
+            payload = {'recipient': {'id': sender}, 'message': {'text': text[::-1] }} # We're going to send this back
             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
         except Exception as e:
             print traceback.format_exc()
