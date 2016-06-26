af027f9b52d2299013eb4e53a6df6570b5fed5d3
Clarence Chan <clarencejy1234@gmail.com>
2016-06-25 20:07:53 -0400

    py indents

--- course.py
+++ course.py
@@ -1,19 +1,19 @@
 import urllib
 
-class course:
-	title = None
-	users = []
-	num = None
-	name = None
+class Course:
+    title = None
+    users = []
+    num = None
+    name = None
 
-
-	def __init__(self, num, name):
+    def __init__(self, num, name):
         self.num = num
         self.name = name
         self.title = name + " " + num
 
     def addUser(self, user):
         self.users.append(user)
+
 
     def update(self):
         getcoursesurl = 'https://api.uwaterloo.ca/v2/courses/'self.name+'/'+self.num+'.json?key='+key
