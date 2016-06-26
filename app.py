b9a867af579aad45a15e9653d0bd376645f3f0d2
Clarence Chan <clarencejy1234@gmail.com>
2016-06-25 21:21:18 -0400

    another one

--- app.py
+++ app.py
@@ -48,6 +48,8 @@ def webhook():
                 if totalcourses[i].title == text:
                     returntext = totalcourses[i].update()
 
+            sys.stdout.write(returntext)
+
             payload = {'recipient': {'id': sender}, 'message': {'text': returntext }} # We're going to send this back
             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
         except Exception as e:
